52a825526ca94cda13e6c822755533c4ca826c81
Marcos <marcos.torrod.1@educa.jcyl.es>
2023-06-09 18:59:23 +0200

    Mi segundo commit

--- src/MarcosGIT/MarcosGIT.java
+++ src/MarcosGIT/MarcosGIT.java
@@ -3,6 +3,7 @@ package ControlVersiones;
 public class Git {
 	public static void main(String[] args) {		
 		System.out.println("Empezando proyecto");
+		System.out.println("Realizando la modificación en GIT");
 	}
 
 }
