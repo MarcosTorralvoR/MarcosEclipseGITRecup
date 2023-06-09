9ae5fcaa5c75b39c093ac9351b3f67832cb6c8db
MarcosTorralvoR <127139240+MarcosTorralvoR@users.noreply.github.com>
2023-06-09 19:14:02 +0200

    commiteando desde GitHub

--- src/MarcosGIT/MarcosGIT.java
+++ src/MarcosGIT/MarcosGIT.java
@@ -4,6 +4,7 @@ public class Git {
 	public static void main(String[] args) {		
 		System.out.println("Empezando proyecto");
 		System.out.println("Realizando la modificación en GIT");
+		System.out.println("línea añadida desde GitHub");
 	}
 
 }
